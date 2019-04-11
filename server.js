e04096b203d49ef0783656f9994883768cbcd746
Uma Abrami <uma.abrami@gmail.com>
2019-04-10 17:27:48 -0700

    Working proxy server, rendering one component, endpoints working

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ var morgan = require('morgan')
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
+let cors = require('cors');
+const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
+app.use(cors());
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/:id', (req, res)=> {
+  console.log(req.params.id);
+  res.sendFile('/Users/umaabrami/desktop/hrr37/hrr37-FEC/Proxy_Uma/public/index.html');
+});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
